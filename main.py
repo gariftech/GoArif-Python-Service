ef47a90ed421775b2522d0e33e2973c5083df4f7
MichaelW <75797622+MagicDash91@users.noreply.github.com>
2025-01-18 11:47:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -112,6 +112,27 @@ from typing import List, Dict, Any
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors()},
+    )
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
+@app.exception_handler(Exception)
+async def all_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": f"Internal Server Error: {str(exc)}"}
+    )
+
 headers = {'Content-Type': 'application/octet-stream'}
 
 if os.getenv("FASTAPI_ENV") == "development":
